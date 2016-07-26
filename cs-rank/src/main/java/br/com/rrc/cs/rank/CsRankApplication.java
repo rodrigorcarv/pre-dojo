9c422a4879bd5868a47a4eec8397a8ad5551832f
rodrigo.carvalho <rodrigo.carvalho@fcamara.com.br>
2016-07-26 00:50:08 -0300

    #Criacao da estrutura inicial do projeto

---
+++ cs-rank/src/main/java/br/com/rrc/cs/rank/CsRankApplication.java
@@ -0,0 +1,12 @@
+package br.com.rrc.cs.rank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsRankApplication.class, args);
+	}
+}
