45c61601a42b120f7303d79c1e83e8564acb2a48
rodrigo.carvalho <rodrigo.carvalho@fcamara.com.br>
2016-07-26 09:47:07 -0300

    Criacao de teste para upload de arquivo

---
+++ cs-rank/src/test/java/br/com/rrc/cs/rank/TestUploadController.java
@@ -0,0 +1,51 @@
+package br.com.rrc.cs.rank;
+
+import static com.jayway.restassured.RestAssured.given;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.jayway.restassured.RestAssured;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CsRankApplication.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
+public class TestUploadController {
+	
+	@Value("${local.server.port}")
+	private int port;
+
+	@Before
+	public void setUp() {
+		RestAssured.port = port;
+	}	
+
+	@Test
+	public void uploadFileTest() throws IOException, URISyntaxException {
+		
+		Path caminho = Paths.get(ClassLoader.getSystemResource("logGame.txt").toURI());
+		File file = caminho.toFile();
+		
+		given().
+			multiPart("file", file).
+		expect().
+				statusCode(HttpStatus.SC_OK).
+		when().
+		        post("/upload");
+	}
+
+}
