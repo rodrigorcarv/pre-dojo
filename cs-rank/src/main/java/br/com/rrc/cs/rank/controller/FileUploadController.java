e131695e83de08f7c765686454daded3c46aa41f
rodrigo.carvalho <rodrigo.carvalho@fcamara.com.br>
2016-07-26 23:26:07 -0300

    Implementacao de upload de arquivo e adicao do log4j

---
+++ cs-rank/src/main/java/br/com/rrc/cs/rank/controller/FileUploadController.java
@@ -0,0 +1,59 @@
+package br.com.rrc.cs.rank.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.stream.Stream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@RestController
+public class FileUploadController {
+	
+	private Log log = LogFactory.getLog(FileUploadController.class);
+	private static final String MENSAGEM_UPLOAD_SUCESSO = "Upload realizado com sucesso";
+	private static final String MENSAGEM_UPLOAD_FALHA  = "Falha ao realizar upload";
+	private static final String MENSAGEM_UPLOAD_ARQUIVO_VAZIO =  "Failed to upload, pois a arquivo informado esta vazio";
+	
+	@SuppressWarnings("resource")
+	@RequestMapping(method = RequestMethod.POST, value = "/upload")
+	public @ResponseBody String fileUpload(@RequestParam("file") MultipartFile multipart, RedirectAttributes redirectAttributes) {
+
+		if (!multipart.isEmpty()) {
+
+			try {
+				
+				String rootPath = System.getProperty("catalina.home");
+				File dir = new File(rootPath + File.separator + "tmpFiles");
+				if (!dir.exists())
+					dir.mkdirs();
+
+				String caminho = String.format("%s%s%s",dir.getAbsolutePath(), File.separator, multipart.getOriginalFilename());
+				
+				File serverFile = new File(caminho);
+				log.debug(String.format("%s", caminho));
+			        
+				Stream<String> linhas = Files.lines(serverFile.toPath());
+				linhas.forEach(System.out::println);
+				
+				redirectAttributes.addFlashAttribute(MENSAGEM_UPLOAD_SUCESSO);
+				log.debug(MENSAGEM_UPLOAD_SUCESSO);
+			} catch (IOException|RuntimeException e) {
+				log.error(String.format("%s%s", MENSAGEM_UPLOAD_FALHA, e.getMessage()), e);
+				redirectAttributes.addFlashAttribute(String.format("%s%s", MENSAGEM_UPLOAD_FALHA, e.getMessage()));
+			}
+		} else {
+			log.error(MENSAGEM_UPLOAD_ARQUIVO_VAZIO);
+			redirectAttributes.addFlashAttribute(MENSAGEM_UPLOAD_ARQUIVO_VAZIO);
+		}
+		return "redirect:/";
+	}
+}
